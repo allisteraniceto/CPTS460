0fbd6d00a801593d4cf73bde74f993e7028ed84e
allisteraniceto <allisteraniceto@gmail.com>
2023-09-13 02:16:14 -0700

    finished enqueue(), dequeue(), printQueue(), scheduler()

--- HW1/hw1/t.c
+++ HW1/hw1/t.c
@@ -172,37 +172,68 @@ put_proc(PROC *p)
 // Case 3: non-empty queue, new process needs to be inserted somewhere in it. look through the processes in the queue until we find a spot where the process' priority will be properly respected.
 
 enqueue(PROC **queue, PROC *p)
-{
+{	
+	PROC *prev, *current;
      // Case 1: empty queue. make the passed process a new queue!
     if ((*queue) == NULL)
     {
+		queue = p; //queue head point first proces
+		p->next = NULL; //no next process
+		return;
     }
     // Case 2: non-empty queue, new process has greatest priority. insert new process onto head of the queue.
     else if (p->priority > (*queue)->priority)
     {
+		p->next = queue; //new process will point to head
+		queue = p; //head will now point to new process
+		return;
     }
     // Case 3: non-empty queue, new process needs to be inserted somewhere in it. look through the processes in the queue until we find a spot where the process' priority will be properly respected.
     else
-    {
+    {	
+		prev = NULL; //used for previous process
+		current = *queue; //head process
+		//traverse through list and compare priorities
+		while(current && p->priority <= current->priority)
+			prev = current; 
+			current = current->next; //compare to next priority
+		}
+		//then insert process in queue
+		prev->next = p;
+		p->next = current;
     }
 }
 
 //Remove a PROC (highest priority in the next homework) 
 //order by priority the first one in queue
 // return its pointer;
-PROC *dequeue(PROC **queue)
-{
+PROC *dequeue(PROC **queue){
     // get the proc we need...
     // and then modify the queue to remove the proc we need...
-    // return the proc we deserve! 
+    // return the proc we deserve!
+
+	PROC *p = *queue; //get pprocess from queue
+	if (*queue != NULL){ //if process is not empty
+		*queue = (*queue)->next;
+	}
+	return p;
 }
 
 //6. print the queue entries in [pid, priority]->  format;
 printQueue(PROC *queue)
 {
-    PROC *p;
+    PROC *p = queue;
     // if we don't have a queue, say "There are no processes in this queue"
     // if we do, print each item in the queue
+	if (queue == NULL){
+		return;
+	}
+	//print queue list
+	while (queue != NULL){
+		printf("[%d, %d]->",p->pid, p->priority);
+		p = p->next;
+	}
+	printf("NULL\n"); //NULL at the end of list
 }
 
 // 7. Scheduler
@@ -213,6 +244,10 @@ Use the MODIFIED scheduler() function propose in class
 int scheduler()
 {
   //Use the MODIFIED scheduler() function propose in class
+	if (running->status == READY){
+		enqueue(&readyQueue, running);
+	}
+	running = dequeue(&readyQueue);
 }
 
 

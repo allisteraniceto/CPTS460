6bb51fbad723780551fef377b72107bd6c25df26
allisteraniceto <allisteraniceto@gmail.com>
2023-11-12 15:35:34 -0800

    redoing main

--- HW3/hw3/t.c
+++ HW3/hw3/t.c
@@ -6,20 +6,16 @@
 // 10. Run the os!
 main()
 {
-    printf("MTX starts in main()\n");
-    initialize();      // initialize and create P0 as running
-    kfork();     // P0 kfork() P1
-    while(1){
-        printf("P0 running\n");
-      
-        if (proc[1].status != READY){
-	        printf("no runable process, system halts\n");            
-        }
-        while(!readyQueue);
-        printf("P0 switch process\n");
-        do_tswitch();   // P0 switch to run P1
-    }
-    printf("Exiting MTX!\n");
+    printf("\nWelcome to the CS460 Multitasking System New User!\n");
+    
+    printf("initializing...");
+    initialize();
+    
+    printf("forking...");
+    kfork();
+    
+    printf("switching...");
+    do_tswitch();
     
     printf("\nGoodbye User!\n");
 }

fdd484cde4586e1f5aeb76602cedc19ef5561597
allisteraniceto <allisteraniceto@gmail.com>
2023-11-12 15:01:55 -0800

    fixing how main() works using solution from HW2

--- HW3/hw3/t.c
+++ HW3/hw3/t.c
@@ -6,16 +6,20 @@
 // 10. Run the os!
 main()
 {
-    printf("\nWelcome to the CS460 Multitasking System New User!\n");
-    
-    printf("initializing...");
-    initialize();
-    
-    printf("forking...");
-    kfork();
-    
-    printf("switching...");
-    tswitch();
+    printf("MTX starts in main()\n");
+    init();      // initialize and create P0 as running
+    kfork();     // P0 kfork() P1
+    while(1){
+        printf("P0 running\n");
+      
+        if (proc[1].status != READY){
+	        printf("no runable process, system halts\n");            
+        }
+        while(!readyQueue);
+        printf("P0 switch process\n");
+        do_tswitch();   // P0 switch to run P1
+    }
+    printf("Exiting MTX!\n");
     
     printf("\nGoodbye User!\n");
 }

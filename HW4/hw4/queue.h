51fbe5e75c73442e237deecbdb886b6cea92852b
allisteraniceto <allisteraniceto@gmail.com>
2023-12-09 14:46:11 -0800

    fixing int definitions

--- HW4/hw4/queue.h
+++ HW4/hw4/queue.h
@@ -4,9 +4,10 @@
 
 PROC *get_proc(PROC **list);
 put_proc(PROC **list,PROC *p);
-enqueue(PROC **queue, PROC *p);
+int enqueue(PROC **queue, PROC *p);
 PROC *dequeue(PROC **queue);
-printQueue(PROC *queue);
+int printQueue(PROC *queue);
 int scheduler();
+int reschedule();
 
 #endif
